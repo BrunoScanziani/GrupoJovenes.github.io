fd5df325a17a1ced05c1d87a2143bc727fa6eab9
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2023-10-11 18:18:21 -0300

    Revert "responsive"
    This reverts commit 89ec670dd79c11a25e4f0020a3fc4db7a6b82594.

--- js/cart.js
+++ js/cart.js
@@ -6,42 +6,59 @@ let container = document.getElementById("productsCarritoContainer");
 document.addEventListener("DOMContentLoaded",() => {
 
     getJSONData(urlCart + id + `.json`).then(function(resultObj){
+        //Creo la cabecera de la lista de productos en el carrito//
+        container.innerHTML += `
+            <div class="row">
+                <div class="col">
+                
+                </div>
+                <div class="col font-weight-bold">
+                Nombre
+                </div>
+                <div class="col font-weight-bold">
+                Costo
+                </div>
+                <div class="col font-weight-bold">
+                Cantidad
+                </div>
+                <div class="col font-weight-bold">
+                Subtotal
+                </div>    
+        </div>
+        <hr id="line">
+        `
         //Agrego el producto al carrito//
         if (resultObj.status === "ok"){
             let data = resultObj.data;
             container.innerHTML += `
-            <div class="d-flex flex-row align-items-center">
-                <div class="col d-flex justify-content-left">
-                    <div class="col-lg-2"></div>
-                    <img src=${data.articles[0].image} class="img-thumbnail col-md-6"></img>
+            <div class="row">
+                <div class="col">
+                <img src=${data.articles[0].image} class="img_cart"></img>
                 </div>
                 <div class="col">
-                    <p>${data.articles[0].name}</p>
+                <p>${data.articles[0].name}</p>
                 </div>
                 <div class="col">
-                    <p>${data.articles[0].currency} ${data.articles[0].unitCost}</p>
+                <p>${data.articles[0].currency} ${data.articles[0].unitCost}</p>
                 </div>
                 <div class="col">
-                    <div class="col-md-4">
-                        <input type="number" class="form-control" id="cantidad" value="1" min="1">
-                    </div>
+                <input type="number" class="counter_cart" id="cantidad" value="1" min="1">
                 </div>
                 <div class="col">
-                    <strong id="subtotal"></strong> 
+                <p id="subtotal"></p> 
                 </div>    
-            </div>
-            <hr style="border: 1px solid lightgray; margin: 12px 0px; color: black;" class="bg-light">
+        </div>
+        <hr id="line">
             `
-            
             
             const precio = data.articles[0].unitCost;
             const cantidadInput = document.getElementById("cantidad");
             const subtotalSpan = document.getElementById("subtotal");
-            subtotalSpan.innerHTML = "USD " + cantidadInput.value * precio;
+            subtotalSpan.innerHTML = "USD" + cantidadInput.value * precio;
 
             cantidadInput.addEventListener("input", () => {
                 subtotalSpan.innerHTML = " ";
-                subtotalSpan.innerHTML = "USD " + cantidadInput.value * precio;
+                subtotalSpan.innerHTML = "USD" + cantidadInput.value * precio;
             });
 
 
