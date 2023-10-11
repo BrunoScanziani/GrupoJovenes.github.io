b61cf6f1ba9161ac969e950710de5301f320c9d0
BrunoScanziani <brunoscanziani1@gmail.com>
2023-10-10 21:08:07 -0300

    Actualizaciones-Carrito

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,72 @@
-let url = "https://japceibal.github.io/emercado-api/user_cart/";
-let a = [];
-let id = "25801";
+let urlCart = "https://japceibal.github.io/emercado-api/user_cart/";
+let arrayIds = JSON.parse(localStorage.getItem("prodCarrito"));
+const id = 25801;
+let container = document.getElementById("productsCarritoContainer");
 
 document.addEventListener("DOMContentLoaded",() => {
-    getJSONData(url+id+`.json`).then(function(resultObj){
+
+    getJSONData(urlCart + id + `.json`).then(function(resultObj){
+        //Creo la cabecera de la lista de productos en el carrito//
+        container.innerHTML += `
+            <div class="row">
+                <div class="col">
+                
+                </div>
+                <div class="col">
+                Nombre
+                </div>
+                <div class="col">
+                Costo
+                </div>
+                <div class="col">
+                Cantidad
+                </div>
+                <div class="col">
+                Subtotal
+                </div>    
+        </div>
+        <hr id="line">
+        `
+        //Agrego el producto al carrito//
         if (resultObj.status === "ok"){
-           a = resultObj.data;
-           console.log(a)
+            let data = resultObj.data;
+            container.innerHTML += `
+            <div class="row">
+                <div class="col">
+                <img src=${data.articles[0].image} class="img_cart"></img>
+                </div>
+                <div class="col">
+                <p>${data.articles[0].name}</p>
+                </div>
+                <div class="col">
+                <p>${data.articles[0].currency} ${data.articles[0].unitCost}</p>
+                </div>
+                <div class="col">
+                <input type="number" class="counter_cart" id="cantidad" value="1" min="1">
+                </div>
+                <div class="col">
+                <p id="subtotal"></p> 
+                </div>    
+        </div>
+        <hr id="line">
+            `
+            
+            const precio = data.articles[0].unitCost;
+            const cantidadInput = document.getElementById("cantidad");
+            const subtotalSpan = document.getElementById("subtotal");
+            subtotalSpan.innerHTML = "USD" + cantidadInput.value * precio;
+
+            cantidadInput.addEventListener("input", () => {
+                subtotalSpan.innerHTML = " ";
+                subtotalSpan.innerHTML = "USD" + cantidadInput.value * precio;
+            });
+
+
         }
     })
+
+
+
+
+
 })
