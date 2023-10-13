281cdb9a7c86f22f6270f196f1df569dbda19d3d
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2023-10-13 00:33:04 -0300

    carrito responsable ea

--- js/cart.js
+++ js/cart.js
@@ -2,8 +2,106 @@ let urlCart = "https://japceibal.github.io/emercado-api/user_cart/";
 let arrayIds = JSON.parse(localStorage.getItem("prodCarrito"));
 const id = 25801;
 let container = document.getElementById("productsCarritoContainer");
-const cartScript = document.getElementById("subtotal")
+let array2 = []
 
+let data = [];
+
+if (!localStorage.getItem("prodCarrito")){
+    getJSONData(urlCart + id + `.json`).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            array2.push({id:resultObj.data.articles[0].id.toString(),cant:1})
+        }
+    })
+}else if( !JSON.parse( localStorage.getItem("prodCarrito").includes(50924) ) ){
+    getJSONData(urlCart + id + `.json`).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            array2.push({id:resultObj.data.articles[0].id.toString(),cant:1})
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded",() => {
+    setTimeout(mostrarCarrito,300)
+})
+
+function mostrarCarrito(){
+    arrayCarrito = JSON.parse(localStorage.getItem("prodCarrito"));
+
+    if (localStorage.getItem("prodCarrito")){
+
+        const contador = [];
+
+        arrayCarrito.forEach(elem => {
+            if (contador[elem] === undefined){
+                contador[elem] = 1;
+            }else{
+                contador[elem]++;
+            }
+        })
+        
+        for(const elem in contador){
+            array2.push({id:elem,cant:contador[elem]})
+        }
+        
+        if( JSON.parse( localStorage.getItem("prodCarrito").includes(50924) ) ){
+            array2.forEach(elem => {
+                if (elem.id == '50924'){
+                    elem.cant++
+                    }
+            })
+        }
+        
+    }
+
+    array2.forEach(elem => {
+        mostrarArt(elem)
+        }
+    )
+}
+
+function mostrarArt(element){
+    getJSONData(PRODUCT_INFO_URL + element.id + ".json").then(function(resultCart){
+        if (resultCart.status === "ok"){
+            let dataCart = resultCart.data;
+            container.innerHTML += `
+                <div class="d-flex flex-row align-items-center">
+                    <div class="col d-flex justify-content-left">
+                        <div class="col-lg-2"></div>
+                        <img src=${dataCart.images[0]} class="img-thumbnail col-md-6"></img>
+                    </div>
+                    <div class="col">
+                        <p>${dataCart.name}</p>
+                    </div>
+                    <div class="col">
+                        <p>${dataCart.currency} ${dataCart.cost}</p>
+                    </div>
+                    <div class="col">
+                        <div class="col-md-4">
+                            <input type="number" class="form-control" id="cantidad${dataCart.id}" value="${element.cant}" min="1" 
+                                        onchange="modifSubtotal(${dataCart.id}, ${dataCart.cost}, '${dataCart.currency}')" 
+                                        oninput="modifSubtotal(${dataCart.id}, ${dataCart.cost}, '${dataCart.currency}')">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <strong id="subtotal${dataCart.id}"></strong> 
+                    </div>    
+                </div>
+                <hr style="border: 1px solid lightgray; margin: 12px 0px; color: black;" class="bg-light">
+                `;
+                
+            let precio = dataCart.cost;
+            let cantidadInput = document.getElementById("cantidad" + dataCart.id);
+            let subtotalSpan = document.getElementById("subtotal" + dataCart.id);
+            let currency = dataCart.currency;
+            subtotalSpan.innerHTML = currency + " " + cantidadInput.value * precio;
+
+            cantidadInput.addEventListener("input", () => {
+                subtotalSpan.innerHTML = " ";
+                subtotalSpan.innerHTML = currency + " " + cantidadInput.value * precio;
+            });
+        };
+    })
+}
 
 function modifSubtotal(id, price, moneda) {
     let cantidadInput = document.getElementById("cantidad" + id);
@@ -12,120 +110,3 @@ function modifSubtotal(id, price, moneda) {
     subtotalSpan.innerHTML = moneda + " " + cantidadInput.value * price;
 
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-
-
-
-    getJSONData(urlCart + id + `.json`).then(function (resultObj) {
-        //Creo la cabecera de la lista de productos en el carrito//
-        container.innerHTML += `
-            <div class="row">
-                <div class="col">
-                
-                </div>
-                <div class="col font-weight-bold">
-                    Nombre
-                </div>
-                <div class="col font-weight-bold">
-                    Costo
-                </div>
-                <div class="col font-weight-bold">
-                    Cantidad
-                </div>
-                <div class="col font-weight-bold">
-                    Subtotal
-                </div>    
-            </div>
-            <hr id="line">
-            `
-        //Agrego el producto al carrito//
-        if (resultObj.status === "ok") {
-            let data = resultObj.data;
-            container.innerHTML += `
-                <div class="row">
-                    <div class="col">
-                        <img src=${data.articles[0].image} class="img_cart"></img>
-                    </div>
-                    <div class="col">
-                        <p>${data.articles[0].name}</p>
-                    </div>
-                    <div class="col">
-                        <p>${data.articles[0].currency} ${data.articles[0].unitCost}</p>
-                    </div>
-                    <div class="col">
-                        <input type="number" class="counter_cart" id="cantidad${data.articles[0].id}" value="1" min="1" 
-                            onchange="modifSubtotal(${data.articles[0].id}, ${data.articles[0].unitCost}, '${data.articles[0].currency}')" 
-                            oninput="modifSubtotal(${data.articles[0].id}, ${data.articles[0].unitCost}, '${data.articles[0].currency}')">
-                    </div>
-                    <div class="col">
-                        <p id="subtotal${data.articles[0].id}"></p> 
-                    </div>    
-                </div>
-                <hr id="line">
-                `
-
-            let precio1 = data.articles[0].unitCost;
-            let cantidadInput1 = document.getElementById("cantidad" + data.articles[0].id);
-            let subtotalSpan1 = document.getElementById("subtotal" + data.articles[0].id);
-            let currency1 = data.articles[0].currency
-
-            subtotalSpan1.innerHTML = currency1 + " " + cantidadInput1.value * precio1;
-
-            cantidadInput1.addEventListener("input", () => {
-                subtotalSpan1.innerHTML = " ";
-                subtotalSpan1.innerHTML = currency1 + " " + cantidadInput1.value * precio1;
-            });
-        }
-    })
-
-    actualizarCantidadEnCarrito()
-
-    if (arrayCarrito.length > 0) {
-        arrayCarrito.forEach(element => {
-            if (element !== 50924) {
-                getJSONData(PRODUCT_INFO_URL + element + ".json").then(function (resultCart) {
-                    if (resultCart.status === "ok") {
-                        let dataCart = resultCart.data;
-                        container.innerHTML += `
-                            <div class="row">
-                                <div class="col">
-                                    <img src=${dataCart.images[0]} class="img_cart"></img>
-                                </div>
-                                <div class="col">
-                                    <p>${dataCart.name}</p>
-                                </div>
-                                <div class="col">
-                                    <p>${dataCart.currency} ${dataCart.cost}</p>
-                                </div>
-                                <div class="col">
-                                    <input type="number" class="counter_cart" id="cantidad${dataCart.id}" value="1" min="1" 
-                                        onchange="modifSubtotal(${dataCart.id}, ${dataCart.cost}, '${dataCart.currency}')" 
-                                        oninput="modifSubtotal(${dataCart.id}, ${dataCart.cost}, '${dataCart.currency}')">
-                                </div>
-                                <div class="col">
-                                    <p id="subtotal${dataCart.id}"></p> 
-                                </div>    
-                            </div>
-                            <hr>
-                            `;
-                        let precio = dataCart.cost;
-                        let cantidadInput = document.getElementById("cantidad" + dataCart.id);
-                        let subtotalSpan = document.getElementById("subtotal" + dataCart.id);
-                        let currency = dataCart.currency
-
-                        subtotalSpan.innerHTML = currency + " " + cantidadInput.value * precio;
-
-                        cantidadInput.addEventListener("input", () => {
-                            subtotalSpan1.innerHTML = " ";
-                            subtotalSpan1.innerHTML = currency + " " + cantidadInput.value * precio;
-                        })
-                    };
-                });
-            }
-        });
-    }
-
-
-
-});
